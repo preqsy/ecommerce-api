b922b8addf210691b44d394d5eac6b2f792b7abd
Precious Ohanyere <obbyprecious24@gmail.com>
2024-06-06 22:01:31 +0100

    added customer endpoint test

--- tests/fixtures/customer_samples.py
+++ tests/fixtures/customer_samples.py
@@ -1,7 +1,7 @@
 def sample_customer_create():
     return {
-        "first_name": "Obinna",
-        "last_name": "Precious",
+        "first_name": "Enzo",
+        "last_name": "Fernandez",
         "username": "preqsy",
         "phone_number": "+2347032937596",
         "auth_id": None,
