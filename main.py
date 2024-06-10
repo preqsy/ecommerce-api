54c1f50efc2c15c4cfd3f95f78b57fb0923beb68
Precious Ohanyere <obbyprecious24@gmail.com>
2024-06-10 14:23:51 +0100

    replaced main code

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+from models import auth_user, product, cart
+from core.db import engine
 from core.middleware import start_up_db
 from endpoints import (
     customer_router,
@@ -8,6 +10,10 @@ from endpoints import (
     product_router,
     cart_router,
 )
+
+auth_user.Base.metadata.create_all(bind=engine)
+product.Base.metadata.create_all(bind=engine)
+cart.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
