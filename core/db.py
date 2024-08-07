a07de5b952b61c9ac1a31f97f551ab1cc0664e02
Precious Ohanyere <obbyprecious24@gmail.com>
2024-08-07 22:59:20 +0100

    updated core.db

--- core/db.py
+++ core/db.py
@@ -2,6 +2,8 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy import create_engine
 from core import settings
 
+
+print(f"Database URL: {settings.SQLALCHEMY_DATABASE_URL}")
 engine = create_engine(url=settings.SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
