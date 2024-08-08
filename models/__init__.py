48e7e13a18af49e1b41a7400927539376c59f3c4
Precious Ohanyere <obbyprecious24@gmail.com>
2024-08-08 13:00:09 +0100

    updated actions

--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,12 @@ from .cart import *
 from .order import *
 from .customer import *
 from .vendor import *
+
+from core.db import engine
+
+auth_user.Base.metadata.create_all(bind=engine)
+product.Base.metadata.create_all(bind=engine)
+cart.Base.metadata.create_all(bind=engine)
+order.Base.metadata.create_all(bind=engine)
+customer.Base.metadata.create_all(bind=engine)
+vendor.Base.metadata.create_all(bind=engine)
